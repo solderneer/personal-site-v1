c5247925f3afb485a66046a4d05fbc9100367b77
Sudharshan <sudharshan@makerforce.io>
2017-02-25 12:26:41 +0800

    added source for website

---
+++ js/gallery.js
@@ -0,0 +1,26 @@
+$(function () {
+		
+	var filterList = {
+	
+		init: function () {
+		
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixItUp({
+				selectors: {
+  			  target: '.portfolio',
+  			  filter: '.filter'	
+  		  },
+  		  load: {
+    		  filter: '.app' // show app tab on first load
+    		}     
+			});								
+		
+		}
+
+	};
+	
+	// Run the show!
+	filterList.init();
+	
+});	
