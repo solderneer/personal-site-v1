08800135abc936e75d209975a267fa9db8588fe1
solderneer <sudhar393@gmail.com>
2017-06-19 11:34:16 +0800

    typed.js prevented repetition

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
-    Typed.new(".element", {
-        strings: ["I love ^1000 electronics.", "I love ^1000 computers.", "I love ^1000 embedded ^200 systems.", "I love ^1000 microcontrollers.",
-        "I love ^1000 PCB ^200 Design.", "I love ^1000 robotics.", "I love ^1000 computer ^200 vision.", "I love ^1000 C/C++."],
-        typeSpeed: 0,
-        loop: true,
-        loopCount: null,
+	Typed.new(".element0", {
+		strings:["I love"],
+		typespeed: 0,
+		loop: false,
+		callback: function(){
+			document.getElementsByClassName('typed-cursor')[0].style.display = 'none';
 
-    });
+			Typed.new(".element1", {
+				strings: ["^1000 electronics.", "^1000 computers.", "^1000 embedded ^200 systems.", "^1000 microcontrollers.",
+				"^1000 PCB ^200 Design.", "^1000 robotics.", "^1000 computer ^200 vision.", "^1000 C/C++."],
+				typeSpeed: 0,
+				loop: true,
+				loopCount: null,
+				backspace: function(curString, curStrPos){
+					self.stopNum = 6;
+				},
+			});
+		}
+	});
 });
