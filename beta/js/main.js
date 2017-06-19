d336e59f33a0b60e18d6e3039ca6d8c1ab9490f2
solderneer <sudhar393@gmail.com>
2017-06-19 11:53:31 +0800

    I'll continue later

--- beta/js/main.js
+++ beta/js/main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 			Typed.new(".element1", {
 				strings: ["^1000 electronics.", "^1000 computers.", "^1000 embedded ^200 systems.", "^1000 microcontrollers.",
-				"^1000 PCB ^200 Design.", "^1000 robotics.", "^1000 computer ^200 vision.", "^1000 C/C++."],
+				"^1000 PCB ^200 design.", "^1000 robotics.", "^1000 computer ^200 vision.", "^1000 C/C++."],
 				typeSpeed: 0,
 				loop: true,
 				loopCount: null,
