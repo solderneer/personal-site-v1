4dfc7d484e2c090d5a96d9a49ec38d648b1b94a1
solderneer <sudhar393@gmail.com>
2017-06-19 10:16:34 +0800

    social nav links

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     Typed.new(".element", {
-        strings: ["I love ^1000 electronics.", "I love ^1000 computers.", "I love ^1000 embedded systems.", "I love ^1000 microcontrollers.",
-        "I love ^1000 PCB Design.", "I love ^1000 robotics.", "I love ^1000 computer vision.", "I love ^1000 C/C++."],
+        strings: ["I love ^1000 electronics.", "I love ^1000 computers.", "I love ^1000 embedded ^200 systems.", "I love ^1000 microcontrollers.",
+        "I love ^1000 PCB ^200 Design.", "I love ^1000 robotics.", "I love ^1000 computer ^200 vision.", "I love ^1000 C/C++."],
         typeSpeed: 0,
         loop: true,
         loopCount: null,
