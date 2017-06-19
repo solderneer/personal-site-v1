66e40a9d036e0b43c55d682ddc9d49f667c8e410
solderneer <sudhar393@gmail.com>
2017-06-19 09:32:44 +0800

    added some typing animations

---
+++ beta/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    Typed.new(".element", {
+        strings: ["I love Science.", "I love Engineering."],
+        typeSpeed: 0,
+        loop: true,
+        loopCount: null
+    });
+});
