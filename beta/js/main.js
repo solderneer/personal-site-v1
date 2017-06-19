a41b95ef5ff3220186c9fa26edba504f928ef57c
solderneer <sudhar393@gmail.com>
2017-06-19 09:38:29 +0800

    more typed.js stuff

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
     Typed.new(".element", {
-        strings: ["I love Science.", "I love Engineering."],
+        strings: ["I love ^1000 Science.", "I love ^1000 Engineering."],
         typeSpeed: 0,
         loop: true,
-        loopCount: null
+        loopCount: null,
+
     });
 });
