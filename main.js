c37a8098c9f1720c1f3de872b95ce4652bc6e8c8
Sudharshan <sudhar393@gmail.com>
2018-07-15 20:11:35 +0800

    awesome

---
+++ main.js
@@ -0,0 +1,18 @@
+var quotes = ["Emotions are abstract logic.",
+              "The world isn't black or white, it is shades of grey.",
+              "Love is simply a bug in the human mind.",
+              "I'm antisocial - there's no question about it.",
+              "The science of today is the technology of tomorrow",
+              "Any sufficiently advanced technology is indistinguishable from magic",
+              "There are no ghosts in the world. Save those we make for ourselves.",
+              "bored.",
+              "Change is the only constant",
+              "Somewhere in between emotional and emotionless"];
+
+var colors = ["#F57F17", "#212121", "#880E4F", "#01579B", "#B71C1C", "#006064", "#3E2723",
+              "#BF360C", "#1B5E20", "#000000"];
+
+var rand = Math.floor(Math.random() * 10);
+
+document.querySelector('#quote').innerText = quotes[rand]
+document.body.style.backgroundColor = colors[rand];
